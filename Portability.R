d0ee0b07deb9261420cde9b4be24eb4c9bb9563b
ibrahimzuniga <ibrahimzuniga@gmail.com>
2026-03-31 10:21:10 -0500

    Add renv.lock, ignore renv folder, add Portability.R instruct

---
+++ Portability.R
@@ -0,0 +1,48 @@
+#setup on environments
+#Portability
+
+# already installed
+# install.packages("renv")
+
+renv::init()
+# welcoome message
+# "Welcome to renv! It looks like this is your first time using renv.
+# This is a one-time message, briefly describing some of renv's functionality."
+
+#important output
+# - Lockfile written to "~/Documents/AneLab/BetterRsoftware_workshop/spacewalks/renv.lock".
+
+#Check installation status
+renv::status()
+
+#install packages withing renv
+renv::install()
+#If you already library the renv package (library(renv)), you don't need to use the renv:: option. 
+
+#Install packages
+#It does install the latest versions.
+renv::install("jsonlite", "lubridate", "ggplot2")
+
+
+#Check installation status
+renv::status()
+#Sometimes you get incompatibility between packages
+
+renv::snapshot()
+#Will update all packages in the lock file. 
+
+#practice uninstall
+remove.packages(c("jsonlite", "lubridate", "ggplot2"))
+
+library("jsonlite")
+# Error in library("jsonlite") : there is no package called ‘jsonlite’
+
+renv::restore()
+# The following package(s) will be updated:
+#   
+#   # CRAN -----------------------------------------------------------------------
+# - jsonlite    [* -> 2.0.0]
+# - lubridate   [* -> 1.9.5]
+
+
+
